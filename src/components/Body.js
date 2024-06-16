8a3e3b79712265f35b82ab2a4db0fd8688acf870
Bikash Yadav <bikashpdyadav11@gmail.com>
2024-06-16 12:51:39 +0530

    This commit is a mistake

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,28 @@
 import RestroCardComponent from "./RestroCardComponent";
 import resList from "../utils/mockData";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const Body = () => {
   const [ListofRestaurants, setListofRestaurants] = useState(resList);
+
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const proxyUrl = 'https://cors.bridged.cc/';
+        const targetUrl = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+        const response = await fetch(proxyUrl + targetUrl);
+        const json = await response.json();
+        console.log(json);
+        setListofRestaurants(json.data.cards); // Adjust based on the actual structure of your JSON response
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
     return (
         <div className="body">
             <div className="filter">
