24e0a53bc66b28470a9cb0c281313b479db23c10
Bikash Yadav <bikashpdyadav11@gmail.com>
2024-06-17 01:11:42 +0530

    CORS error resolved

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,25 +4,16 @@ import {useEffect, useState} from "react";
 
 const Body = () => {
   const [ListofRestaurants, setListofRestaurants] = useState(resList);
-
   
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const proxyUrl = 'https://cors.bridged.cc/';
-        const targetUrl = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
-        const response = await fetch(proxyUrl + targetUrl);
-        const json = await response.json();
-        console.log(json);
-        setListofRestaurants(json.data.cards); // Adjust based on the actual structure of your JSON response
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
+    const fetchData = async () =>{
+      const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      const data = await response.json();
+      console.log(data);
+    }
     fetchData();
-  }, []);
-
+  }
+  , []);
     return (
         <div className="body">
             <div className="filter">
