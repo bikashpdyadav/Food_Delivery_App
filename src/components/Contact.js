364aab8b87dea6e361d9c96d82d29c73bd898393
bikashpdyadav <bikashpdyadav11@gmail.com>
2024-10-17 20:16:14 +0530

    Footer finalised

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,25 +2,28 @@ import { LOGO_URL } from "../utils/constants";
 
 const Contact = () => {
     return (
-        <div className="bg-pink-100 flex flex-col md:flex-row justify-between items-start md:items-center p-6 md:p-8 text-black">
-            <div className="flex flex-col md:w-1/2 mb-4 md:mb-0">
-                <p className='p-1 font-semibold'>Contact Us</p>
-                <p className="ml-4">+91 8252587109</p>
-                <p className="ml-4">team@food_delivery_app.com</p>
-                <p className='p-1 font-semibold'>Customer Support Available</p>
-                <p className='p-1'>Available 24hr</p>
-                <p className='p-1 font-semibold'>Find Us Here</p>
-                <div className="flex gap-2 p-1">
-                    <img src="https://th.bing.com/th?id=OIP.NUFU5mhqhqOr82Ge-CwjawHaHv&w=244&h=255&c=8&rs=1&qlt=90&r=0&o=6&pid=3.1&rm=2" alt="Instagram" className="w-6 h-6"></img>
-                    <img src="https://th.bing.com/th?id=OIP.biG9UdrkNGTbvX1YvOvY4AHaHa&w=250&h=250&c=8&rs=1&qlt=90&r=0&o=6&pid=3.1&rm=2" alt="Twitter" className="w-6 h-6"></img>
-                    <img src="https://th.bing.com/th?id=OIP.6uTQ7mOjYOD2sNKxUdnaNAHaHa&w=250&h=250&c=8&rs=1&qlt=90&r=0&o=6&pid=3.1&rm=2" alt="LinkedIn" className="w-6 h-6"></img>
-                    <img src="https://th.bing.com/th?id=OIP.cOz92GK9w_2_VxUIWBL0ngHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="Facebook" className="w-6 h-6"></img>
+        <div className="bg-pink-100 flex flex-col md:flex-row justify-between items-center md:items-center p-6 md:p-8 text-black">
+            {/* Contact Us Section */}
+            <div className="flex flex-col items-center md:items-start md:w-1/2 mb-4 md:mb-0">
+                <p className='p-1 font-semibold text-center md:text-left'>Contact Us</p>
+                <p className="ml-0 text-center md:text-left">+91 8252587109</p>
+                <p className="ml-0 text-center md:text-left">team@food_delivery_app.com</p>
+                <p className='p-1 font-semibold text-center md:text-left'>Customer Support Available</p>
+                <p className='p-1 text-center md:text-left'>Available 24hr</p>
+                <p className='p-1 font-semibold text-center md:text-left'>Find Us Here</p>
+                <div className="flex gap-2 p-1 justify-center md:justify-start">
+                    <img src="https://th.bing.com/th?id=OIP.NUFU5mhqhqOr82Ge-CwjawHaHv&w=244&h=255&c=8&rs=1&qlt=90&r=0&o=6&pid=3.1&rm=2" alt="Instagram" className="w-6 h-6" />
+                    <img src="https://th.bing.com/th?id=OIP.biG9UdrkNGTbvX1YvOvY4AHaHa&w=250&h=250&c=8&rs=1&qlt=90&r=0&o=6&pid=3.1&rm=2" alt="Twitter" className="w-6 h-6" />
+                    <img src="https://th.bing.com/th?id=OIP.6uTQ7mOjYOD2sNKxUdnaNAHaHa&w=250&h=250&c=8&rs=1&qlt=90&r=0&o=6&pid=3.1&rm=2" alt="LinkedIn" className="w-6 h-6" />
+                    <img src="https://th.bing.com/th?id=OIP.cOz92GK9w_2_VxUIWBL0ngHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="Facebook" className="w-6 h-6" />
                 </div>
             </div>
-            <div className='flex flex-col md:w-1/2'>
-                <img src={LOGO_URL} alt="Food Delivery App Logo" className='w-1/4 mb-4 md:mb-0'></img>
-                <p className='font-semibold'>Regd. Address</p>
-                <p className="leading-relaxed">
+
+            {/* Logo and Address Section */}
+            <div className='flex flex-col items-center md:items-start md:w-1/2'>
+                <img src={LOGO_URL} alt="Food Delivery App Logo" className='w-1/4 mb-4 md:mb-0' />
+                <p className='font-semibold text-center md:text-left'>Regd. Address</p>
+                <p className="leading-relaxed text-center md:text-left">
                     BDA, AMBEDKAR INSTITUTE OF TECHNOLOGY, Outer Ring Rd, Near Gnana Bharati, 2<sup>nd</sup> Stage, Nagarbhavi, Bengaluru, Karnataka, 560056
                 </p>
             </div>
