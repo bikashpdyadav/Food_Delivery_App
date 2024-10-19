183d226bc90fb4c99212038ac246eaeffaec5e30
bikashpdyadav <bikashpdyadav11@gmail.com>
2024-10-19 19:25:46 +0530

    Navigation improved

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -168,12 +168,13 @@ const ConfirmationModal = ({ onConfirm, onCancel }) => {
 };
 
 const SignInFirst = ({ onCancel }) => {
+    const navigate = useNavigate();
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
             <div className="bg-white p-6 rounded-lg shadow-xl shadow-black max-w-sm w-full z-50">
                 <h1 className="text-2xl font-bold mb-4">Please sign in first</h1>
                 <button
-                    onClick={() => window.location.href = '/login'}
+                    onClick={() => navigate('/login')}
                     className="px-4 py-2 bg-blue-600 text-white text-lg rounded mb-2 w-full hover:bg-blue-700"
                 >
                     Sign In
